f4ed77611894a6ba7a90390b5e0867d53cdb17ea
renjie.wang <renjie.wang@tongdun.cn>
2017-03-29 23:49:42 +0800

    message ok

--- src/main/java/com/loavne/wsip/worker/message/MessageWorker.java
+++ src/main/java/com/loavne/wsip/worker/message/MessageWorker.java
@@ -3,18 +3,12 @@ package com.loavne.wsip.worker.message;
 import com.loavne.wsip.protocol.msg.SipRequestMsg;
 import com.loavne.wsip.protocol.msg.SipStatusMsg;
 import com.loavne.wsip.protocol.header.HeaderKeys;
-import com.loavne.wsip.util.MapUtils;
 import com.loavne.wsip.worker.AbstractWorker;
 import com.loavne.wsip.worker.registrar.contact.ContactContext;
 import com.loavne.wsip.worker.registrar.contact.ContactHolder;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
-import java.util.UUID;
 
 /**
  * Created by wangrenjie on 17/3/26.
@@ -35,29 +29,18 @@ public class MessageWorker extends AbstractWorker{
             logger.error("contact:{} not found",contact);
             return;
         }
-        ChannelFuture f = contactContext.getChannel().writeAndFlush(msg);
-        f.addListener(new ChannelFutureListener() {
-            public void operationComplete(ChannelFuture channelFuture) throws Exception {
-                SipStatusMsg statusMsg = new SipStatusMsg(msg.getVersion(),"200","Ok");
-                Map<String,String> headers = statusMsg.getHeaders();
-                headers.put(HeaderKeys.KEY_CONTENT_LENGTH,"0");
-                MapUtils.copy(msg.getHeaders(), headers, new MapUtils.ValueFilter() {
-                            public String newValue(String key, String value) {
-                                //To
-                                if(HeaderKeys.KEY_TO.equals(key)){
-                                    return "<" + value + ">;tag=" + UUID.randomUUID().toString().replaceAll("-","");
-                                }
-                                return value;
-                            }
-                        }, HeaderKeys.KEY_VIA,
-                        HeaderKeys.KEY_CALL_ID,
-                        HeaderKeys.KEY_CSEQ,
-                        HeaderKeys.KEY_FROM,
-                        HeaderKeys.KEY_TO);
-                ctx.writeAndFlush(statusMsg);
-
-            }
-        });
+        contactContext.getChannel().writeAndFlush(msg);
     }
 
+    @Override
+    public void innerWork(ChannelHandlerContext ctx, SipStatusMsg msg) {
+        String from = msg.getHeaders().get(HeaderKeys.KEY_FROM);
+        String contact = from.substring(from.indexOf("<") + 1, from.indexOf(">"));
+        ContactContext contactContext = ContactHolder.getOkContactContext(contact);
+        if(null == contactContext){
+            logger.error("contact:{} not found ",contact);
+            return;
+        }
+        contactContext.getChannel().writeAndFlush(msg);
+    }
 }
