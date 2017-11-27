566d85b881e22ff71cf9361bb8ed1710a40bf38d
renjie.wang <renjie.wang@tongdun.cn>
2017-11-27 18:18:59 +0800

    auth

---
+++ src/main/java/com/loavne/wsip/user/UserManager.java
@@ -0,0 +1,28 @@
+package com.loavne.wsip.user;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * Created by wangrenjie on 17/11/27.
+ */
+public class UserManager {
+
+    private static final Map<String,String> USER_MAP = Maps.newHashMap();
+
+    static{
+        USER_MAP.put("rj","rj");
+        USER_MAP.put("001","001");
+        USER_MAP.put("002","002");
+        USER_MAP.put("003","003");
+        USER_MAP.put("004","004");
+        USER_MAP.put("005","005");
+        USER_MAP.put("006","006");
+        USER_MAP.put("007","007");
+    }
+
+    public static String getPassWord(String userName){
+        return USER_MAP.get(userName);
+    }
+}
--- src/main/java/com/loavne/wsip/worker/registrar/contact/ContactStateUnauthorized.java
+++ src/main/java/com/loavne/wsip/worker/registrar/contact/ContactStateUnauthorized.java
@@ -1,15 +1,22 @@
 package com.loavne.wsip.worker.registrar.contact;
 
+import com.google.common.collect.Maps;
 import com.loavne.wsip.protocol.msg.SipRequestMsg;
 import com.loavne.wsip.protocol.msg.SipStatusMsg;
 import com.loavne.wsip.protocol.header.HeaderKeys;
+import com.loavne.wsip.user.UserManager;
 import com.loavne.wsip.util.Constants;
 import com.loavne.wsip.util.MapUtils;
+import com.loavne.wsip.util.Md5;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -19,15 +26,15 @@ public class ContactStateUnauthorized implements ContactState{
 
     private Logger logger = LoggerFactory.getLogger(ContactStateUnauthorized.class);
 
+    private static final Pattern PATTERN = Pattern.compile("(.*?)=\"(.*?)\"");
+
     public SipStatusMsg state(SipRequestMsg msg) {
         String authorization = msg.getHeaders().get(HeaderKeys.KEY_AUTHENTICATION);
-        if(null == authorization){
-            logger.error("authorization is null");
-            return null;
-        }
-        if(!auth(authorization)){
-            logger.error("authorization invalid");
-            return null;
+        if(null == authorization || !auth(authorization)){
+            logger.error("authorization failed");
+            ContactContext contactContext = ContactHolder.getContactContext(msg.getContact());
+            contactContext.setContactState(new ContactStateInit());
+            return contactContext.handle(msg);
         }
         SipStatusMsg result = new SipStatusMsg(msg.getVersion(),"200","OK");
         Map<String,String> headers = result.getHeaders();
@@ -52,6 +59,36 @@ public class ContactStateUnauthorized implements ContactState{
     }
 
     private boolean auth(String authorization){
-        return true;
+        Map<String,String> map = Maps.newHashMap();
+        String[] array = authorization.split(",");
+        for(String temp : array){
+            Matcher matcher = PATTERN.matcher(temp);
+            if(!matcher.matches()){
+                continue;
+            }
+            String key = matcher.group(1);
+            String value = matcher.group(2);
+            Constants.KEYLIST.forEach(new Consumer<String>() {
+                @Override
+                public void accept(String s) {
+                    if(key.contains(s)){
+                        map.put(s,value);
+                    }
+                }
+            });
+        }
+        try{
+            String password = UserManager.getPassWord(map.get(Constants.USERNAME));
+
+            String response = Md5.encode(Md5.encode(map.get(Constants.USERNAME) + ":" +
+                    map.get(Constants.REALM) + ":" + password) + ":" + map.get(Constants.NONCE) + ":" + Md5.encode("REGISTER:" + map.get(Constants.URI)));
+            if(response.equals(map.get(Constants.RESPONSE))){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (Exception e){
+            return false;
+        }
     }
 }
